417c9a67c1bbecf04d2c9ff5536ca09d5cf15f6f
Viral <viral.citrusbug@gmail.com>
2022-01-28 14:31:39 +0530

    POPUP on news button included

--- Reverb.js
+++ Reverb.js
@@ -1,3 +1,5 @@
+
+//POPUP on Explore Button
 function popupfunction(){
     var i = document.getElementById('popupdiv');
     i.style.display = "block";
@@ -6,4 +8,16 @@ function popupfunction(){
 function popupNone(){
     var i = document.getElementById("popupdiv");
     i.style.display = "none";
+}
+
+//POPUP on News button
+function popupnews(){
+    var i = document.getElementById('popupnews');
+    i.style.display = "block";
+}
+
+function popupnonenews(){
+    var i = document.getElementById('popupnews');
+    i.style.display = "none";
+    
 }
