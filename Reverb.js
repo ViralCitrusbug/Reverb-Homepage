e9f40b5e0d850c5514f6757feea3a215f63db7f9
Viral <viral.citrusbug@gmail.com>
2022-01-28 13:08:23 +0530

    Added JS

---
+++ Reverb.js
@@ -0,0 +1,9 @@
+function popupfunction(){
+    var i = document.getElementById('popupdiv');
+    i.style.display = "block";
+    
+}
+function popupNone(){
+    var i = document.getElementById("popupdiv");
+    i.style.display = "none";
+}
